a487033bfd43a1b2528addeb9252cd8773be03f5
Wei Wang <chengtian.wang@gmail.com>
2015-06-22 18:44:51 -0700

    Fix CommandLineMachineWideSettings default path on CoreCLR temporarily

--- src/Microsoft.Framework.PackageManager/NuGet/CommandLine/CommandLineMachineWideSettings.cs
+++ src/Microsoft.Framework.PackageManager/NuGet/CommandLine/CommandLineMachineWideSettings.cs
@@ -18,7 +18,7 @@ namespace NuGet
 #if DNX451
             var baseDirectory = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
 #else
-            var baseDirectory = Environment.GetEnvironmentVariable("ProgramData");
+            var baseDirectory = "/usr/local/share/";
 #endif
             _settings = new Lazy<IEnumerable<NuGet.Settings>>(
                 () => NuGet.Settings.LoadMachineWideSettings(
